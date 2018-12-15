e0c49f2cb4617b705317b0804b9e60afb0d57607
mmikolajczak <mkm0796@gmail.com>
2018-12-15 22:13:22 +0100

    day 6, part 1, improved, more optimal solution

--- 6_1/solution.py
+++ 6_1/solution.py
@@ -1,10 +1,7 @@
-import math
-from collections import Counter
+from collections import defaultdict
 
 
 INPUT_PATH = 'input.txt'
-GRID_PADDING = 100
-_MULTIPLE_CLOSEST_PTS = 'multi_pts'
 
 
 def flatten_one_level(nested):
@@ -19,27 +16,6 @@ def manhattan_distance(vec1, vec2):
     return sum(abs(el_v1 - el_v2) for el_v1, el_v2 in zip(vec1, vec2))
 
 
-def construct_grid(cords, grid_padding=101):
-    cords_xs = tuple(pt[0] for pt in cords)
-    cords_ys = tuple(pt[1] for pt in cords)
-    min_x, max_x = min(cords_xs), max(cords_xs)
-    min_y, max_y = min(cords_ys), max(cords_ys)
-    grid = [[None for x in range(min_x - grid_padding, max_x + grid_padding + 1)]
-            for y in range(min_y - grid_padding, max_y + grid_padding + 1)]
-    return grid
-
-
-def find_closest_points(grid, cords):
-    closest_pts_grid = grid.copy()
-    for y in range(len(grid)):
-        for x in range(len(grid[0])):
-            pt_to_cords_dist = tuple((cord, manhattan_distance(cord, (x, y))) for cord in cords)
-            pt_to_cords_dist = sorted(pt_to_cords_dist, key=lambda el: el[1])
-            closest_pts_grid[y][x] = (pt_to_cords_dist[0][0] if pt_to_cords_dist[0][1] != pt_to_cords_dist[1][1]
-                                      else _MULTIPLE_CLOSEST_PTS)
-    return closest_pts_grid
-
-
 def normalize_cords(cords):
     cords_xs = tuple(pt[0] for pt in cords)
     cords_ys = tuple(pt[1] for pt in cords)
@@ -48,13 +24,53 @@ def normalize_cords(cords):
     return {(pt[0] - min_x, pt[1] - min_y) for pt in cords}
 
 
-def count_up_closest(cords, grid_padding):
-    grid_points = construct_grid(cords, grid_padding)
-    cords_norm = normalize_cords(cords)
-    closest_pts_grid = find_closest_points(grid_points, cords_norm)
-    closest_pts_counts = Counter(flatten_one_level(closest_pts_grid))
-    del closest_pts_counts[_MULTIPLE_CLOSEST_PTS]
-    return closest_pts_counts
+def cords_system_start_to_0_0(cords):
+    cords_xs = tuple(pt[0] for pt in cords)
+    cords_ys = tuple(pt[1] for pt in cords)
+    min_x = min(cords_xs)
+    min_y = min(cords_ys)
+    return [(pt[0] - min_x, pt[1] - min_y) for pt in cords]
+
+
+def construct_closest_grid(cords):
+    cords_xs = tuple(pt[0] for pt in cords)
+    cords_ys = tuple(pt[1] for pt in cords)
+    min_x, max_x = min(cords_xs), max(cords_xs)
+    min_y, max_y = min(cords_ys), max(cords_ys)
+    grid = [[None for x in range(min_x, max_x + 1)]
+            for y in range(min_y, max_y + 1)]
+
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            pt_to_cords_dist = tuple((cord, manhattan_distance(cord, (x, y))) for cord in cords)
+            pt_to_cords_dist = sorted(pt_to_cords_dist, key=lambda el: el[1])
+            min_dist = pt_to_cords_dist[0][1]
+            grid[y][x] = []
+            for pt, dist in pt_to_cords_dist:
+                if dist == min_dist:
+                    grid[y][x].append(pt)
+                else:
+                    break
+    return grid
+
+
+def filter_out_cords_closest_to_edges(closest_grid, cords):
+    edge_points = closest_grid[0] + closest_grid[-1]
+    for row in closest_grid[1: -1]:
+        edge_points.append(row[0])
+        edge_points.append(row[-1])
+    edge_points = flatten_one_level(edge_points)
+    filtered = [pt for pt in cords if pt not in edge_points]
+    return filtered
+
+
+def calculate_cords_area(closest_grid, cords):
+    cords_area = defaultdict(int)
+    for y in range(len(closest_grid)):
+        for x in range(len(closest_grid[0])):
+            if len(closest_grid[y][x]) == 1 and closest_grid[y][x][0] in cords:
+                cords_area[closest_grid[y][x][0]] += 1
+    return cords_area
 
 
 def solution_6_1(input_path):
@@ -65,15 +81,16 @@ def solution_6_1(input_path):
     for line in content.split('\n'):
         x, y = line.split(', ')
         cords.append((int(x), int(y)))
-    # assumption - closest points are counted for a grid with some padding, if for a grid
-    # with padding greater (+1) than that count for particular coordinate is greater - it
-    # is counted as infinity.
-    coords_closest_pts_counts_grid_norm = count_up_closest(cords, GRID_PADDING)
-    coords_closest_pts_counts_grid_ext = count_up_closest(cords, GRID_PADDING + 1)
-    for key in coords_closest_pts_counts_grid_norm:
-        if coords_closest_pts_counts_grid_norm[key] != coords_closest_pts_counts_grid_ext[key]:
-            coords_closest_pts_counts_grid_norm[key] = math.inf
-    res = max(v for v in coords_closest_pts_counts_grid_norm.values() if v != math.inf)
+    # method goes as follows:
+    # - for each point in grid, calculate the closest cords
+    # - for future processing, exclude cords that are closest to any point of grid edges (infinite)
+    # - calculate area for remaining cords
+    # - find maximum
+    cords = cords_system_start_to_0_0(cords)
+    grid_pts_closest_cords = construct_closest_grid(cords)
+    cords = filter_out_cords_closest_to_edges(grid_pts_closest_cords, cords)
+    cords_area = calculate_cords_area(grid_pts_closest_cords, cords)
+    res = max(cords_area.values())
     return res
 
 
